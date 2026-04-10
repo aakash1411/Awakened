30ad44dc0ef3d8b4c94b630e78a88f6636d00bce
Aakash <aakashs1411@gmail.com>
2026-04-09 22:28:45 -0700

    Initial Commit

---
+++ AwakenedTests/AwakenedTests.swift
@@ -0,0 +1,19 @@
+//
+//  AwakenedTests.swift
+//  AwakenedTests
+//
+//  Created by Aakash on 4/9/26.
+//
+
+import Testing
+@testable import Awakened
+
+struct AwakenedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
